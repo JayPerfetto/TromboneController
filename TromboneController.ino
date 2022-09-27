b5f0632e6871ecca7d410c12b8fb03ce7984acc5
The MasterChief <34031697+JayPerfetto@users.noreply.github.com>
2022-09-26 21:02:46 -0700

    cleanup + refactoring

--- TromboneController.ino
+++ TromboneController.ino
@@ -1,55 +1,64 @@
 // ---------------------------------------------------------------- //
-// Arduino Ultrasoninc Sensor HC-SR04
-// Re-writed by Arbi Abdul Jabbaar
-// Using Arduino IDE 1.8.7
-// Using HC-SR04 Module
-// Tested on 17 September 2019
+// Arduino Leonardo TromboneChamp Controller Code
+// Author: Jason Perfetto
+// Using Arduino IDE 2.0.0
+// Using HC-SR04 Module, Arduino Leonardo, Press-Button
+// Tested on 26 September 2022
 // ---------------------------------------------------------------- //
-
-#define echoPin 3   // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 4    //attach pin D3 Arduino to pin Trig of HC-SR04
-#define buttonPin 2  // attach pin D4 to the Button
-#define ledPin 13    // attach pin D4 to the Button
 #include <AbsMouse.h>
 #include <Keyboard.h>
 
-// defines variables
-int buttonState = 0;
-long duration;   // variable for the duration of sound wave travel
-float distance;  // variable for the distance measurement
-float lastDistance;
-float maxDistance = 75.0;  // max range for trombone slide
-float minDistance = 5.0;   // min range for trombone slide
-const int yMax = 1080;
+#define echoPin 3    // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 4    //attach pin D3 Arduino to pin Trig of HC-SR04
+#define buttonPin 2  // attach pin D4 to the Button
+#define ledPin 13    // 13 is the pin designation for the onboard LED on the Arduino chip
+
+// instance variables
+int buttonState = 0;    // initialize button as un-pressed
+long duration;          // duration of sound wave travel
+float distance;         // distance measurement
+const int yMax = 1080;  // max screen resolution in Y axis
+const int xMax = 1920;  // max screen resolution in X axis
 const int yMin = 0;
-const int xMax = 1920;
 const int xMin = 0;
-const float dMax = 37.0;
-const float dMin = 4.0;
-float dRange = dMax - dMin;
-float yStep = yMax / dRange;
-float xMid = xMax / 2;
+const float dMax = 37.0;  // max allowable range for distance sensor (cm)
+const float dMin = 4.0;   // min allowable range for distance sensor (cm)
+
+float dRange = dMax - dMin;   // total potential trombone slide travel distance
+float yStep = yMax / dRange;  // ratio of pixels to distance for translating slide position to mouse position
+float xMid = xMax / 2;        // midpoint of the screen horizontally
 
 void setup() {
-  pinMode(trigPin, OUTPUT);   // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT);    // Sets the echoPin as an INPUT
-  pinMode(buttonPin, INPUT);  // Sets the button as an INPUT
-  Serial.begin(9600);
-  AbsMouse.init(xMax, yMax);
-  Keyboard.begin();         // // Serial Communication is starting with 9600 of baudrate speed
+  initButton();
+  initDistanceSensor();
 }
 
 void loop() {
   listenForButtonClick();
   listenToDistanceSensor();
+  moveMouse();
+}
+
+void initButton() {
+  pinMode(buttonPin, INPUT);  // Sets the button as an INPUT
+}
+
+void initDistanceSensor() {
+  pinMode(trigPin, OUTPUT);  // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT);   // Sets the echoPin as an INPUT
+  AbsMouse.init(xMax, yMax);
+  Keyboard.begin();
+  Serial.begin(9600);  // Serial Communication is starting with 9600 of baudrate speed
+}
+
+void moveMouse() {
   if (distance <= dMax && distance >= dMin) {
-    float yActual = (yStep * distance) - 100;
+    float yActual = (yStep * distance) - 100; // 100 pixel shift-down to accomodate notches / improve gameplay calibration - adjust if needed 
     AbsMouse.move(xMid, yActual);
   }
 }
 
 void listenToDistanceSensor() {
-  lastDistance = distance;
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -60,15 +69,9 @@ void listenToDistanceSensor() {
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = float(duration * 0.034 / 2.0);
-  float diff = distance - lastDistance;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  Serial.print("Diff: ");
-  Serial.println(diff);
+  distance = distance();
+  logDistance();
 }
-
 
 void listenForButtonClick() {
   buttonState = digitalRead(buttonPin);
@@ -81,5 +84,15 @@ void listenForButtonClick() {
     // turn LED off:
     digitalWrite(ledPin, LOW);
     Keyboard.release(32);
+  }
+
+  float distance() {
+    return float(duration * 0.034 / 2.0);
+  }
+
+  void logDistance() {
+    // Prints the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.println(distance);
   }
 }
